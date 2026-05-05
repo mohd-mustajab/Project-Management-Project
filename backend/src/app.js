62995ec6cc12411aecc8277feda012261754d437
Mohd Mustajab <mohdmustajab002@gmail.com>
2026-05-05 19:16:27 +0530

    update backend scripts and CORS configuration; change API base URL in frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,12 +8,19 @@ const dashboardRoutes = require("./routes/dashboard.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://project-management-project.onrender.com"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 module.exports = app;
